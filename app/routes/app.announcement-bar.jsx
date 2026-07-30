e829254da375c4e291c495976506d4220e33c807
Maharob Hossain <maharobhossain8880@gmail.com>
2026-07-30 21:32:29 +0600

    Initial commit: Shopify Sales Booster app

---
+++ app/routes/app.announcement-bar.jsx
@@ -0,0 +1,9 @@
+export default function AnnouncementBar() {
+  return (
+    <s-page heading="Announcement Bar">
+      <s-section heading="Create Announcement">
+        <s-paragraph>announcement bar design</s-paragraph>
+      </s-section>
+    </s-page>
+  );
+}
---
+++ app/routes/app.countdown-timer.jsx
@@ -0,0 +1,9 @@
+export default function CountdownTimer() {
+  return (
+    <s-page heading="Countdown Timer Bar">
+      <s-section heading="Create Timer">
+        <s-paragraph>countdown timer design</s-paragraph>
+      </s-section>
+    </s-page>
+  );
+}
