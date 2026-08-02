e5082ecc383216836e2bae460c17cc55b1cfa058
Maharob Hossain <maharobhossain8880@gmail.com>
2026-08-02 21:46:31 +0600

    Add close function on announcment bar function

---
+++ prisma/migrations/20260802152509_add_dismissible/migration.sql
@@ -0,0 +1,21 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Announcement" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "shop" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "link" TEXT,
+    "bgColor" TEXT NOT NULL DEFAULT '#202223',
+    "textColor" TEXT NOT NULL DEFAULT '#ffffff',
+    "fontSize" INTEGER NOT NULL DEFAULT 14,
+    "isActive" BOOLEAN NOT NULL DEFAULT false,
+    "dismissible" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+INSERT INTO "new_Announcement" ("bgColor", "createdAt", "fontSize", "id", "isActive", "link", "message", "shop", "textColor", "updatedAt") SELECT "bgColor", "createdAt", "fontSize", "id", "isActive", "link", "message", "shop", "textColor", "updatedAt" FROM "Announcement";
+DROP TABLE "Announcement";
+ALTER TABLE "new_Announcement" RENAME TO "Announcement";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
